2decbd457593a22e1dbed21afcdd0f7728c3c842
Vinayak Patil <30465977+Vingithub93@users.noreply.github.com>
2018-01-24 13:08:40 +0530

    a

--- src/generic/Auto_constant.java
+++ src/generic/Auto_constant.java
@@ -7,5 +7,6 @@ String GECKO_VALUE="./software/geckodriver.exe";
 String CHROME_KEY="webdriver.chrome.driver";
 String CHROME_VALUE="./software/chromedriver.exe";
 
+  String CONFIG_PATH="./config.properties";
 
 }
